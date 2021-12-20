04dee6be40df4fc98688086c2aa107c482b80668
TranThiThuy1951061046 <thuymia3008@gmail.com>
2021-12-20 22:45:18 +0700

    signup

--- frontend/signup.php
+++ frontend/signup.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Singin_Netflix</title>
+    <title>Signup_Netflix</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/styles_login.css">
+    <link rel="stylesheet" href="css/signup.php">
 </head>
 
 <body class="bg__wrap">
@@ -15,7 +15,6 @@
         <a href="#" class="navbar-brand">
             <img src="images/logo.jpg" alt="net/logo" class="img-fluid">
         </a>
-
     </nav>
     <div class="container">
         <div class="col-lg-4 col-md-8 col-sm-10 col-12 mx-lg-auto mx-md-auto mx-5m-auto mx-auto">
@@ -63,50 +62,7 @@
         </div>
     </div>
 
-    <div class="footer">
-      <div class="footer-head">
-        <a href="#" class="footer-head-text">Bạn có câu hỏi? Liên hệ với chúng tôi.</a>
-      </div>
-      <ul class="footer-links structural">
-        <li class="footer-link-item" placeholder="footer_responsive_link_faq_item">
-          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/support/412" placeholder="footer_responsive_link_faq">
-            <span id="" data-uia="data-uia-footer-label">Câu hỏi thường gặp</span>
-          </a>
-        </li>
-            
-        <li class="footer-link-item" placeholder="footer_responsive_link_help_item">
-          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com" placeholder="footer_responsive_link_help">    
-            <span id="" data-uia="data-uia-footer-label">Trung tâm trợ giúp</span>
-          </a>
-        </li>
-      
-        <li class="footer-link-item" placeholder="footer_responsive_link_terms_item">
-          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/legal/termsofuse" placeholder="footer_responsive_link_terms">   
-            <span id="" data-uia="data-uia-footer-label">Điều khoản sử dụng</span>  
-          </a>  
-        </li>
-      </ul>
-      <ul class="footer-links structural">
-        <li class="footer-link-item" placeholder="footer_responsive_link_privacy_separate_link_item">
-          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/legal/privacy" placeholder="footer_responsive_link_privacy_separate_link">
-            <span id="" data-uia="data-uia-footer-label">Quyền riêng tư</span>
-          </a>
-        </li>
-      
-        <li class="footer-link-item" placeholder="footer_responsive_link_cookies_separate_link_item">
-          <a class="footer-link" data-uia="footer-link" href="#" placeholder="footer_responsive_link_cookies_separate_link">
-            <span id="" data-uia="data-uia-footer-label">Tùy chọn cookie</span>
-          </a>
-        </li>
-      
-        <li class="footer-link-item" placeholder="footer_responsive_link_corporate_information_item">
-          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/legal/corpinfo" placeholder="footer_responsive_link_corporate_information">
-            <span id="" data-uia="data-uia-footer-label">Thông tin doanh nghiệp</span>
-          </a>
-        </li>
-      </ul>
-    </div>  
-
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
