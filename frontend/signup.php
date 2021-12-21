f083af6eb7fc2bf8eeb092faa71f757e3d9cdf90
TranThiThuy1951061046 <thuymia3008@gmail.com>
2021-12-21 10:16:02 +0700

    Sửa signup

--- frontend/signup.php
+++ frontend/signup.php
@@ -7,55 +7,48 @@
     <title>Signup_Netflix</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/signup.php">
+    <link rel="stylesheet" href="css/style_signup.css">
 </head>
-
+ 
 <body class="bg__wrap">
     <nav class="main_nav">
         <a href="#" class="navbar-brand">
             <img src="images/logo.jpg" alt="net/logo" class="img-fluid">
         </a>
+        <button>Signin</button>
     </nav>
     <div class="container">
         <div class="col-lg-4 col-md-8 col-sm-10 col-12 mx-lg-auto mx-md-auto mx-5m-auto mx-auto">
             <div class="form__data">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-12 mx-lg-auto mx-md-auto mx-5m-auto mx-auto">
                     <div class="form_title">
-                        <h1>Đăng nhập</h1>
+                        <h1>Tạo tài khoản để bắt đầu xem Netflix</h1>
                     </div>
                     <form action="#" method="GET">
                             <div class="mb-3">
+                              <div id="email" class="form-text">Họ</div>
                               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email hoặc số điện thoại">
-                              <div id="email" class="form-text">Vui lòng nhập email hoặc số điện thoại hợp lệ.</div>
                             </div>
                             <div class="mb-3">
+                               <div id="password" class="form-text">Tên</div>
                               <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
-                              <div id="password" class="form-text">Mật khẩu của bạn phải chứa từ 4 đến 60 ký tự.</div>
                             </div>
-                            <button type="submit" class="btn ">Đăng nhập</button>
-                            
-                            <div class="form__footer__setting">
-                                <div class="form-check form-check-inline">
+                            <div class="mb-3">
+                              <div id="email" class="form-text">Email</div>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email hoặc số điện thoại">
+                            </div>
+                            <div class="mb-3">
+                               <div id="password" class="form-text">Mật khẩu</div>
+                              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
+                            </div>
+                            <div class="form-check form-check-inline">
                                     <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                                    <label class="form-check-label text-white" for="inlineCheckbox1">Ghi nhớ tôi</label>
-                                  </div>
-                                  <div class="form-check form-check-inline">
-                                    <label class="form-check-label text-white" for="inlineCheckbox2">Bạn cần trợ giúp?</label>
-                                  </div>
+                                    <label class="form-check-label" for="inlineCheckbox1">Ghi nhớ tôi</label>
                             </div>
-                            <div class="fb_link">
-                                <a href="#"><img src="images/fb.jpg" class="img-fruid" alt="fb/img"></a>
-                                <label class="form-check-label-fb text-white" for="inlineCheckbox2">Đăng nhập bằng tài khoản Facebook</label>
-                            </div>
-                            <div>
-                                <label class="form-check-label-1 text-white " for="inlineCheckbox2">Bạn mới tham gia Netflix?</label>
-                                <a class="form-dk" href="#">Đăng ký ngay</a>
-                            </div>
-                            <div class="form-research">
-                                <p style="font-size: 14px; color: #757575;" class="rap-form">Trang này được Google reCAPTCHA bảo vệ để đảm bảo bạn không phải là robot.
-                                    <a href="#" class="text-primary">Tìm hiểu thêm</a>
-                                </p>
-                            </div>
+
+                            <button type="submit" class="btn ">Đăng ký</button>
+                            
+                            
                     </form>
                 </div>
             </div>
