ccf796ce43bf7b76beca77af4bd0e3deb8edccb4
CRABHOLO <thuc13072001@gmail.com>
2021-12-22 20:08:54 +0700

    22/12

---
+++ frontend/signup.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup_Netflix</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/css/style_signup.css">
+</head>
+ 
+<body class="bg__wrap">
+    <nav class="main_nav">
+        <a href="#" class="navbar-brand">
+            <img src="assets/images/logo.jpg" alt="net/logo" class="img-fluid">
+        </a>
+        <button class="btn_login">
+        <a href="login.php" class="text_login" >Login</a>
+        </button>
+        
+    </nav>
+    <div class="container">
+        <div class="col-lg-4 col-md-8 col-sm-10 col-12 mx-lg-auto mx-md-auto mx-5m-auto mx-auto">
+            <div class="form__data">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-12 mx-lg-auto mx-md-auto mx-5m-auto mx-auto">
+                    <div class="form_title">
+                        <h1>Tạo tài khoản để bắt đầu xem Netflix</h1>
+                    </div>
+                    <form action="#" method="GET">
+                            <div class="mb-3">
+                              <div class="form-text">Họ</div>
+                              <input  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email hoặc số điện thoại">
+                            </div>
+                            <div class="mb-3">
+                               <div class="form-text">Tên</div>
+                              <input  class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
+                            </div>
+                            <div class="mb-3">
+                              <div id="email" class="form-text">Email</div>
+                              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email hoặc số điện thoại">
+                            </div>
+                            <div class="mb-3">
+                               <div id="password" class="form-text">Mật khẩu</div>
+                              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mật khẩu">
+                            </div>
+                            <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                    <label class="form-check-label" for="inlineCheckbox1">Ghi nhớ tôi</label>
+                            </div>
+
+                            <button type="submit" class="btn ">Đăng ký</button>
+                            
+                            
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
