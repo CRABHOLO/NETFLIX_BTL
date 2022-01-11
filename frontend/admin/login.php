b4499925dc4b50b9d7c345a6a7fe358c12bed5b4
TranThiThuy1951061046 <thuymia3008@gmail.com>
2022-01-11 15:31:00 +0700

    css_login_admin

--- frontend/admin/login.php
+++ frontend/admin/login.php
@@ -1,80 +1,103 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Signin_Admin_Netflix</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/signin.css">
-    
 </head>
-<body>
-    <header>
-        <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">TLU</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+
+<body class="bg__wrap">
+    <nav class="main_nav">
+        <a href="#" class="navbar-brand">
+            <img src="images/logo.jpg" alt="net/logo" class="img-fluid">
+        </a>
+    </nav>
+    <div class="container">
+        <div class="col-lg-4 col-md-8 col-sm-10 col-12 mx-lg-auto mx-md-auto mx-5m-auto mx-auto">
+            <div class="form__data">
+                <div>
+                    <div class="form_title">
+                        <h1>Đăng nhập Admin</h1>
+                    </div>
+                    <form action="process_login.php" method="post">
+                            <div class="mb-3">
+                              <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email hoặc số điện thoại">
+                              <div id="email" class="form-text" >Vui lòng nhập email hoặc số điện thoại hợp lệ.</div>
+                            </div>
+                            <div class="mb-3">
+                              <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Mật khẩu">
+                              <div id="password" class="form-text">Mật khẩu của bạn phải chứa từ 4 đến 60 ký tự.</div>
+                            </div>
+                            <button type="submit" class="btn " name="submit">Đăng nhập</button>
+                            
+                            <div class="form__footer__setting">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                    <label class="form-check-label text-white" for="inlineCheckbox1">Ghi nhớ tôi</label>
+                                    <?php
+                                      if(isset($_GET['error'])){
+                                      echo "<h5 style='color:red'> {$_GET['error']} </h5>";
+                                      }
+                                    ?>
+                                  </div>
+                                  <div class="form-check form-check-inline">
+                                    <label class="form-check-label text-white" for="inlineCheckbox2">Bạn cần trợ giúp?</label>
+                                  </div>
+                            </div>
+                    </form>
                 </div>
             </div>
-            </nav>
         </div>
-    </header>
-    <main>
-        <div class="container">
-        <form class="form-signin" action="process-login.php" method="post" name="login" obsubmit="return validateLogin" >
-            
-            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" name="txtEmail" class="form-control" placeholder="Email address" required autofocus>
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" name="txtPass" class="form-control" placeholder="Password" required>
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-                <?php
-                    if(isset($_GET['error'])){
-                        echo "<h5 style='color:red'> {$_GET['error']} </h5>";
-                    }
+    </div>
 
-                ?>
-            </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit" name="btnSignIn" >Sign in</button>
-            <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
-        </form>
-        </div>    
-    </main>
+    <div class="footer">
+      <div class="footer-head">
+        <a href="#" class="footer-head-text">Bạn có câu hỏi? Liên hệ với chúng tôi.</a>
+      </div>
+      <ul class="footer-links structural">
+        <li class="footer-link-item" placeholder="footer_responsive_link_faq_item">
+          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/support/412" placeholder="footer_responsive_link_faq">
+            <span id="" data-uia="data-uia-footer-label">Câu hỏi thường gặp</span>
+          </a>
+        </li>
+            
+        <li class="footer-link-item" placeholder="footer_responsive_link_help_item">
+          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com" placeholder="footer_responsive_link_help">    
+            <span id="" data-uia="data-uia-footer-label">Trung tâm trợ giúp</span>
+          </a>
+        </li>
+      
+        <li class="footer-link-item" placeholder="footer_responsive_link_terms_item">
+          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/legal/termsofuse" placeholder="footer_responsive_link_terms">   
+            <span id="" data-uia="data-uia-footer-label">Điều khoản sử dụng</span>  
+          </a>  
+        </li>
+      </ul>
+      <ul class="footer-links structural">
+        <li class="footer-link-item" placeholder="footer_responsive_link_privacy_separate_link_item">
+          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/legal/privacy" placeholder="footer_responsive_link_privacy_separate_link">
+            <span id="" data-uia="data-uia-footer-label">Quyền riêng tư</span>
+          </a>
+        </li>
+      
+        <li class="footer-link-item" placeholder="footer_responsive_link_cookies_separate_link_item">
+          <a class="footer-link" data-uia="footer-link" href="#" placeholder="footer_responsive_link_cookies_separate_link">
+            <span id="" data-uia="data-uia-footer-label">Tùy chọn cookie</span>
+          </a>
+        </li>
+      
+        <li class="footer-link-item" placeholder="footer_responsive_link_corporate_information_item">
+          <a class="footer-link" data-uia="footer-link" href="https://help.netflix.com/legal/corpinfo" placeholder="footer_responsive_link_corporate_information">
+            <span id="" data-uia="data-uia-footer-label">Thông tin doanh nghiệp</span>
+          </a>
+        </li>
+      </ul>
+    </div>  
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
