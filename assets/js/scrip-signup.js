37434937982a3bb61f141c79e1c8072a4bc80570
TranThiThuy1951061046 <thuymia3008@gmail.com>
2022-01-12 21:39:40 +0700

    sửa ajax

--- assets/js/scrip-signup.js
+++ assets/js/scrip-signup.js
@@ -5,23 +5,24 @@ $(document).ready(function(){
          let emailPatterm = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(gmail.com)$/;
          if(emailPatterm.test($(this).val()) == false){
              $("#emailHelp").text("Email không hợp lệ ").css("color","red");
-         }else{
-            $.ajax ({
-                url:"checkmail-signup.php",
-                type:"post",
-                data:{emaill:$(this).val()},
-                success:function(res){
-                    $("#emailHelp").text(res).css("color","blue");
-                }
-            })
          }
+        //  else{
+        //     $.ajax ({
+        //         url:"checkmail-signup.php",
+        //         type:"post",
+        //         data:{emaill:$(this).val()},
+        //         success:function(res){
+        //             $("#emailHelp").text(res).css("color","blue");
+        //         }
+        //     })
+        //  }
      })
    })
    $(document).ready(function(){
      $("#inputpassword1").change(function(){
          let passPatterm = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
          if(passPatterm.test($("#inputpassword1").val()) == false){
-             $("#pass1Help").text("Mật khẩu Tối thiểu tám ký tự, ít nhất một chữ cái viết hoa, chữ cái viết thường và chữsố").css("color","red");
+             $("#pass1Help").text("Mật khẩu tối thiểu tám ký tự, ít nhất một chữ cái viết hoa, chữ cái viết thường và chữ số").css("color","red");
          }else{
             $.ajax ({
                 url:"checkpass-signup.php",
